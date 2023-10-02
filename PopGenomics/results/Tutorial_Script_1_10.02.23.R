aad87c84a653ca961c93169aee6dacadff4de13e
Alex <123191518+beetlesnstuff@users.noreply.github.com>
2023-10-02 11:18:09 -0400

    changes

--- PopGenomics/results/Tutorial_Script_1_10.02.23.R
+++ PopGenomics/results/Tutorial_Script_1_10.02.23.R
@@ -6,18 +6,20 @@ list.files()
 
 # read in the selection stats
 
-s <- npyLoad("allRS_poly.selection.npy")
+s<-npyLoad("allRS_poly.selection.npy")
 
+# convert test statistic to p-value
 pval <- as.data.frame(1-pchisq(s,1))
-head(pval)
+names(pval) = "p_PC1"
 
-names(pval) =c("p_PC1", "p_PC2")
-
-#associate these with our SNP meta-data
-
-p <- read.table("allRS_poly_mafs.sites", sep="\t", header=T, stringsAsFactors = T)
+## read positions
+p <- read.table("allRS_poly_mafs.sites",sep="\t",header=T, stringsAsFactors=T)
+dim(p)
 
 p_filtered = p[which(p$kept_sites==1),]
+dim(p_filtered)
+
+# How many sites got filtered out when testing for selection? Why?
 
 ## make manhattan plot
 plot(-log10(pval$p_PC1),
@@ -26,14 +28,33 @@ plot(-log10(pval$p_PC1),
      ylab="-log10(p-value)",
      main="Selection outliers: pcANGSD e=1 (K2)")
 
-sel_contig <- p_filtered[which(pval==min(pval$p_PC1)), c("chromo", "position")]
+# We can zoom in if there's something interesting near a position...
 
-cutoff=1e-3   # equals a 1 in 5,000 probability
-outlier_contigs <- p_filtered[which(pval<cutoff),c("chromo","position")]
-outlier_contigs
+plot(-log10(pval$p_PC1[2e05:2.01e05]),
+     col=p_filtered$chromo, 
+     xlab="Position", 
+     ylab="-log10(p-value)", 
+     main="Selection outliers: pcANGSD e=1 (K2)")
+
+# get the contig with the lowest p-value for selection
+sel_contig <- p_filtered[which(pval==min(pval$p_PC1)),c("chromo","position")]
+sel_contig
+
+# get all the outliers with p-values below some cutoff
+cutoff=1e-4   # equals a 1 in 5,000 probability
+outlier_contigs <- p_filtered[which(pval$p_PC1<cutoff),c("chromo","position")]
+
+outlier_contig <- outlier_contigs[which(outlier_contigs$position>0),]
 
 # how many outlier loci < the cutoff?
 dim(outlier_contigs)[1]
 
 # how many unique contigs harbor outlier loci?
 length(unique(outlier_contigs$chromo))
+
+write.table(unique(outlier_contigs$chromo), 
+            "allRS_poly_PC1_outlier_contigs.txt",
+            sep="\t",
+            quote=F,
+            row.names=F,
+            col.names=F)
