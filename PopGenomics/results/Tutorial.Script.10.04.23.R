16f7d03dca4ebf106356e51e3e3eb917687e2059
Alex <123191518+beetlesnstuff@users.noreply.github.com>
2023-10-04 10:52:23 -0400

    change in file name

--- PopGenomics/results/Tutorial.Script.10.04.23.R
+++ PopGenomics/results/Tutorial.Script.10.04.23.R
@@ -8,3 +8,18 @@ library(corrplot)
 bio <- getData("worldclim",var="bio",res=10)
 
 coords <- read.csv("https://www.uvm.edu/~kellrlab/forClass/colebrookSampleMetaData.csv", header=T)
+
+names <- read.table("allRS_bam.list")
+names <- unlist(strsplit(basename(as.character(names[,1])), split = ".sorted.rmdup.bam"))
+split = strsplit(names, "_")
+pops <- data.frame(names[1:95], do.call(rbind, split[1:95]))
+names(pops) = c("Ind", "Pop", "Row", "Col")
+
+angsd_coords <- merge(pops, coords, by.x="Ind", by.y="Tree")
+
+points <- SpatialPoints(angsd_coords[c("Longitude","Latitude")])
+
+clim <- extract(bio,points)
+
+angsd_coords_clim <- cbind.data.frame(angsd_coords,clim)
+str(angsd_coords_clim)
