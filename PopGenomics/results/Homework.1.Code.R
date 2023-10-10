aaf3daa86d824790c6c5d969636cc06b5e8a7d57
Alex <123191518+beetlesnstuff@users.noreply.github.com>
2023-10-10 12:07:51 -0400

    Update Homework.1.Code.R

--- PopGenomics/results/Homework.1.Code.R
+++ PopGenomics/results/Homework.1.Code.R
@@ -3,6 +3,10 @@ setwd("/Users/alexanderkissonergis/Documents/GitHub/ecologicalGenomics/PopGenomi
 library(ggplot2) # plotting
 library(ggpubr) # plotting
 library(RcppCNPy)
+library(raster)
+library(FactoMineR)
+library(factoextra)
+library(corrplot)
 
 COV <- as.matrix(read.table("allRS_poly.cov")) # read in the genetic covariance matrix
 
@@ -66,7 +70,7 @@ ggscatter(data, x = "V1", y = "V2",
 
 # import the ancestry scores (these are the .Q files)
 
-q <- read.table("allRS_poly.admix.2.Q", sep=" ", header=F)
+q <- read.table("allRS_poly.admix.3.Q", sep=" ", header=F)
 
 K=dim(q)[2] #Find the level of K modeled
 
@@ -136,4 +140,79 @@ write.table(unique(outlier_contigs$chromo),
             sep="\t",
             quote=F,
             row.names=F,
+            col.names=F)
+
+COV <- as.matrix(read.table("allRS_poly.cov"))
+
+PCA <- eigen(COV)
+
+data=as.data.frame(PCA$vectors)
+data=data[,c(1:2)] # the second number here is the number of PC axes you want to keep
+
+write.table(data,
+            "allRS_poly_genPC1_2.txt",
+            sep="\t",
+            quote=F,
+            row.names=F,
+            col.names=F)
+
+bio <- getData("worldclim",var="bio",res=10)
+
+coords <- read.csv("https://www.uvm.edu/~kellrlab/forClass/colebrookSampleMetaData.csv", header=T)
+
+#The chunk below refers to your bamlist file that you transferred during last week's PCA/admixture analysis.  It should be the same one you want to use here -- if your sample list for analysis changes in the future, you'll need a different bamlist!
+  
+names <- read.table("allRS_bam.list")
+names <- unlist(strsplit(basename(as.character(names[,1])), split = ".sorted.rmdup.bam"))
+split = strsplit(names, "_")
+pops <- data.frame(names[1:95], do.call(rbind, split[1:95]))
+names(pops) = c("Ind", "Pop", "Row", "Col")
+
+angsd_coords <- merge(pops, coords, by.x="Ind", by.y="Tree")
+
+points <- SpatialPoints(angsd_coords[c("Longitude","Latitude")])
+
+clim <- extract(bio,points)
+
+angsd_coords_clim <- cbind.data.frame(angsd_coords,clim)
+str(angsd_coords_clim)
+
+# Make the climate PCA:
+
+clim_PCA = PCA(angsd_coords_clim[,15:33], graph=T)
+
+# Get a screeplot of cliamte PCA eigenvalues
+
+fviz_eig(clim_PCA)
+
+# What is the climate PCA space our red spruce pops occupy?
+
+fviz_pca_biplot(clim_PCA, 
+                geom.ind="point",
+                col.ind = angsd_coords_clim$Latitude, 
+                gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+                title="Climate PCA (Bioclim)",
+                legend.title="Latitude")
+
+# Which variables show the strongest correlation on the first 2 climate PC axes?
+
+dimdesc(clim_PCA)[1:2]
+
+# Replace "XX" with your bio variable most significant on climate PC1:
+
+write.table(scale(angsd_coords_clim["bio12"]),
+            "allRS_bio12.txt",
+            sep="\t",
+            quote=F,
+            row.names = F,
+            col.names=F)
+
+
+# Replace "YY" with your bio variable most significant on climate PC2:  
+
+write.table(scale(angsd_coords_clim["bio10"]),
+            "allRS_bio10.txt",
+            sep="\t",
+            quote=F,
+            row.names = F,
             col.names=F)
